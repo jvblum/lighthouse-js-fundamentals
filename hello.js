ded6c19f73fbece0fbfee26b41686737d7803495
jvblum <jvmell.skch@gmail.com>
2021-08-25 12:52:16 -0400

    add isEven.js; modify hello.js

--- hello.js
+++ hello.js
@@ -1,5 +1,28 @@
+/*
 const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 
 sayHello("Mr. Monster");
+*/
+
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+// prints
+
+/*
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+// returns 
+*/
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
+// returns & prints
